186bf286b267d0c256e64c244face7fc39c7780c
horobchyk <corneliusbreede@gmail.com>
2022-01-24 16:39:47 +0100

    1. Tag

--- app.js
+++ app.js
@@ -17,17 +17,8 @@ console.time('Hello')
   console.log('Hello') 
 console.timeEnd ('Hello') 
 console.clear();
-let 
-  userName = 'Cornelius',
-  userAge = 39;
-  const userBirthday = '29.12.1982';
-console.log(userName+' ist '+userAge +' Jahre alt und geboren am: '+userBirthday)
-/*Taskt */
-/*let name = 'John';
-let admin = name;
-console.log(name)
-alert(admin)
-*/
+
+
 /*
 console.clear();
 console.log(39+33);
@@ -37,12 +28,55 @@ let a = 10
 console.log(100*a-1)
 let b = a*10
 console.log(a*b+b);*/
-
+/*
 const numbers = [1,2,3,4,5,6]
 console.log(numbers)
 numbers.push(60000)
 console.log(numbers)
+console.log(typeof numbers)
+const hobbies = ['movies', 'music']
+console.log(hobbies)
+console.log(typeof hobbies)*/
 
 /*
 Multiline
 */
+/*
+let val;
+val = String(555);
+val = String(555+555)
+val1 = Number (new Date())
+//
+console.log(val)
+console.log (typeof val)
+console.log (val.length)
+console.log (val1.toFixed(2))
+*/
+
+const intro = "Hallo "
+console.log(intro)
+const intro2 = ' Herr Maus'
+console.log(intro2)
+const joined = `Hallo ${intro2}`
+console.log(joined)
+console.clear();
+let a = 5
+let b = 10
+result = a + b
+//alert(result);
+console.clear()
+//lert("2">"12")
+
+
+const name1  = "Cornelius";
+const name2 = "Breede";
+let val;
+val = Math.floor(Math.random() * 100 + 1)
+console.log(val)
+let name;
+name = name1 + name2;
+nameA = name1 + " " + name2  
+console.log(name)
+console.log(nameA)
+
+
