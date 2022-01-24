4570a9aa6d40531e7a31a68b6b69a6da3c8703a5
horobchyk <corneliusbreede@gmail.com>
2022-01-24 14:08:59 +0100

    Einstieg

---
+++ app.js
@@ -0,0 +1,48 @@
+//Log to console
+/*
+console.log('Hello') // String
+console.log(123+123) // Number
+console.log(true) // Borlean
+var greetings = 'Variable Hallo'; 
+console.log (greetings) //Variable
+console.log({a:1,b:2})
+console.table({a:1,b:2})
+console.error ('This is some error')
+console.warn('This is a warning')
+console.time('Hello')
+  console.log('Hello') 
+  console.log('Hello') 
+  console.log('Hello') 
+  console.log('Hello')
+  console.log('Hello') 
+console.timeEnd ('Hello') 
+console.clear();
+let 
+  userName = 'Cornelius',
+  userAge = 39;
+  const userBirthday = '29.12.1982';
+console.log(userName+' ist '+userAge +' Jahre alt und geboren am: '+userBirthday)
+/*Taskt */
+/*let name = 'John';
+let admin = name;
+console.log(name)
+alert(admin)
+*/
+/*
+console.clear();
+console.log(39+33);
+console.log((4+6+9)/77);
+console.log(3,5,9,12);
+let a = 10
+console.log(100*a-1)
+let b = a*10
+console.log(a*b+b);*/
+
+const numbers = [1,2,3,4,5,6]
+console.log(numbers)
+numbers.push(60000)
+console.log(numbers)
+
+/*
+Multiline
+*/
