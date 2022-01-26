c309e883f82a31f314dd8288c59bf48e5c0a0867
horobchyk <corneliusbreede@gmail.com>
2022-01-26 20:50:50 +0100

    3.Tag

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ console.log (typeof val)
 console.log (val.length)
 console.log (val1.toFixed(2))
 */
-
+/*
 const intro = "Hallo "
 console.log(intro)
 const intro2 = ' Herr Maus'
@@ -78,5 +78,166 @@ name = name1 + name2;
 nameA = name1 + " " + name2  
 console.log(name)
 console.log(nameA)
+*/
+/*
+const name ="Cornelius";
+const age = 30;
+const job = "Verkäufer";
+const city = "München";
+*/
+
+// 
+
+//Text einfügen in HTML ${XXX}
+/*
+html = `
+<ul>
+<li>Name: ${name}</li>
+<li>Age: ${age}</li>
+<li>Job: ${job}</li>
+<li>City: ${city}</li>
+<li>City: ${2 +2} ${city}</li>
+</ul>
+`
+document.body.innerHTML = html
+
+//Arrays
+numbers = [2,3,36,60,43, 23]
+val = numbers.indexOf(36)
+
+//Sortieren
+val=numbers.sort(function(x,y){return x-y})
+
+console.log(numbers)
+console.log(val)
+
+*/
+/*
+let val;
+
+const today = new Date();
+let birthday = new Date("12/29/1982");
+val = today.getMonth()+1;
+
+console.log(val);
+*/
+
+/*
+const val = 1
+if(typeof val !== 'undefined'){
+  console.log('CORRECT')
+}
+else{console.log("INCORRECT")}
+*/
+// Ternery  OPERATOR
+
+/*
+let id = 101
+console.log(id === 100 ? "CORRECT" : "INCORRECT")
+
+*/
+/*
+let userName = prompt("Who's there?", '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass === 'TheMaster') {
+    alert( 'Welcome!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}*/
+/*
+const select = document.querySelector('select');
+const para = document.querySelector('p');
 
 
+select.addEventListener('change', setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  if (choice === 'sunny') {
+    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+  
+  } else if (choice === 'rainy') {
+    para.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+  } else if (choice === 'snowing') {
+    para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+  } else if (choice === 'overcast') {
+    para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+  } else {
+    para.textContent = '';
+  }
+ 
+}*//*
+const select = document.querySelector('select');
+const html = document.querySelector('html');
+const style = document.querySelector('p')
+document.body.style.padding = '10px';
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+  style.remove(class) = 
+}
+
+
+select.addEventListener('change', () => ( select.value === 'black' ) ? update('black','white') : update('white','black'));
+*/
+/*
+const myText = 'I am a string';
+const newString = myText.replace('string', 'sausage');
+console.log(newString);
+const myArray = ['I', 'love', 'chocolate', 'frogs'];
+const madeAString = myArray.join(' ');
+console.log(madeAString);
+
+const myNumber = Math.floor(Math.random() * 49 +1);
+console.log(myNumber) 
+
+
+function draw() {
+  ctx.clearRect(0,0,WIDTH,HEIGHT);
+  for (let i = 0; i < 100; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,0,0,0.5)';
+    ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
+function random(number) {
+  return Math.floor(Math.random()*number);
+}
+draw();
+*/
+/*
+const myText = document.getElementsByClassName('is--displaynone');
+console.log(typeof text)
+const newString = myText.replace('is--displaynone', '');
+
+*//*
+const myArray = ['I', 'love', 'chocolate', 'frogs'];
+const madeAString = myArray.join();
+console.log(madeAString);
+// returns 'I love chocolate frogs'
+
+function hello(name='Chris') {
+  console.log(`Hello ${name}!`);
+}
+
+hello('Ari'); // Hello Ari!
+hello();      // Hello Chris!*/
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener('keydown', event => output.textContent = `You pressed "${event.key}".`);
